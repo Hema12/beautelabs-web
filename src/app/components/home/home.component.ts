dbfeba626d1668290188b658184fbfb4ecd69279
sivagami-cbotsolutions <sivagami@cbotsolutions.com>
2019-12-28 15:57:25 +0530

    resposive home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -65,4 +65,40 @@ showappt() {
    const element1 = document.querySelector('#apptview');
    element1.scrollIntoView({ behavior: 'smooth', block: 'center'});
 }
+showpartner() {
+  // document.getElementById("apptview").scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const id = 'partnerview';
+  const yOffset = -10; 
+  const element = document.getElementById(id);
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({top: y, behavior: 'smooth'});
+   //IE
+   document.getElementById('partnerview').scrollTop = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+   const element1 = document.querySelector('#partnerview');
+   element1.scrollIntoView({ behavior: 'smooth', block: 'center'});
+}
+showhire() {
+  // document.getElementById("apptview").scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const id = 'hireview';
+  const yOffset = -10; 
+  const element = document.getElementById(id);
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({top: y, behavior: 'smooth'});
+   //IE
+   document.getElementById('hireview').scrollTop = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+   const element1 = document.querySelector('#hireview');
+   element1.scrollIntoView({ behavior: 'smooth', block: 'center'});
+}
+showtesti() {
+  // document.getElementById("apptview").scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const id = 'testimonialview';
+  const yOffset = -10; 
+  const element = document.getElementById(id);
+  const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+  window.scrollTo({top: y, behavior: 'smooth'});
+   //IE
+   document.getElementById('testimonialview').scrollTop = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+   const element1 = document.querySelector('#testimonialview');
+   element1.scrollIntoView({ behavior: 'smooth', block: 'center'});
+}
 }
